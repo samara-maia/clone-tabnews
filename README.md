bb62c049ac04919e7c60c88f1a4892d06c32d567
Samara Maia <72771371+samara-maia@users.noreply.github.com>
2024-03-13 21:43:16 +0000

    add scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 O projeto do https://curso.dev visa aprimorar conhecimentos ao implementar o https://www.tabnews.com.br.
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-function Home () {
-    return <h1>Obrigada por fazer parte na minha vida e acredita em mim!</h1>
-} 
+function Home() {
+  return <h1>Obrigada por fazer parte na minha vida e acredita em mim!</h1>;
+}
 export default Home;
