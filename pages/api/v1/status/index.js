297b14806fb51bfd1f6444bdcec53919e5f2e5a1
Samara Maia <72771371+samara-maia@users.noreply.github.com>
2024-05-15 18:16:32 +0000

    add  and  files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sun;");
+  console.log(result.rows);
   response
     .status(200)
     .json({ chave: "alunos do curso.dev são acima da média" });
