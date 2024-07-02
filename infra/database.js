bbba2bfcc3d21040f4cee942b01e622b2e96fb13
Samara Maia <72771371+samara-maia@users.noreply.github.com>
2024-07-02 20:20:22 +0000

    add a more robust ssl configuration method

--- infra/database.js
+++ infra/database.js
@@ -7,16 +7,9 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
+    ssl: getSSLValues(),
   }); // comando sicrono
-  console.log("Credenciais do Postgres:", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  });
-
+  
   try {
     await client.connect(); // comando assicrono
     const result = await client.query(queryObject);
@@ -32,3 +25,13 @@ async function query(queryObject) {
 export default {
   query: query,
 };
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "production" ? true : false;
+}
